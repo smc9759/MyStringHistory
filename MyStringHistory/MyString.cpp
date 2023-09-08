88f5fdae44236f3e64f6d0e29bce61b7c61e027d
smc9759 <42694805+smc9759@users.noreply.github.com>
2023-09-08 16:21:18 +0900

    mfc

---
+++ MyStringHistory/MyString.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string.h>
+
+class MyString {
+private:
+	int string_length;
+	char* string_context;
+public:
+	//int return_length const{ return string_length; }
+	//MyString& 
+};
